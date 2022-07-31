c4d75bba6c61dc0cbe5007797996b7cdedf513eb
Demaris Ashmore <demiashmore@yahoo.com>
2022-07-30 22:07:38 -0600

    commit tree 92ec08698ee86c2c2815e9d4c35329027d18210c

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function moveDodgerLeft() {
   });
 
   function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
   
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
     }
   }
